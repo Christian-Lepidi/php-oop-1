8f3dea9065822e2eb66a118264aecf6de803647f
Christian-Lepidi <christianlepidi@gmail.com>
2024-03-07 17:31:58 +0100

    feat: add. files subdivision

--- index.php
+++ index.php
@@ -1,34 +1,9 @@
 <?php 
 
-class Production{
-   public $title;
-   public $language;
-   public $vote;
-   public function __construct($title, $language, $vote){
-
-    $this->title = $title;
-    $this->language = $language;
-    $this->vote = $vote;
-  }
-  public function getText(){
-    return (string)$this->title. $this->language. $this->vote."";
-  }
-}
-
-$letal_wheapon = new Production(" Letal Wheapon"," English", 7);
+require_once __DIR__."/Models/productions.php";
+require_once __DIR__."/Models/db.php";
 
 
-
-$the_lord_of_the_ring = new Production(" The Lord of the Ring"," English", 9);
-
-$true_detective = new Production(" True Detective","English", 7.5);
-
-
-$productions = [
-  $letal_wheapon,
-  $the_lord_of_the_ring,
-  $true_detective,
-];
 ?>
 
 <!DOCTYPE html>
